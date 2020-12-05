275d1ed484befa451bbbd35e6fec28d26ba6ccc9
Danil Nikonov <dan.nikonov@gmail.com>
2020-12-05 16:44:21 +0300

    clean code

--- index.php
+++ index.php
@@ -13,29 +13,3 @@ echo "\n";
 echo "anwser 2: " . $task->solution_2();
 echo "\n";
 $task->answer();
-
-/*
-
-
-
-
-
-
-
-
- /usr/bin/php /home/dn/code/adventofcode/index.php -y 2019 -d 1
- /usr/bin/php /home/dn/code/adventofcode/index.php -y 2019 -d 2
- /usr/bin/php /home/dn/code/adventofcode/index.php -y 2019 -d 3
-
- /usr/bin/php /home/dn/code/adventofcode/index.php -y 2020 -d 1
- /usr/bin/php /home/dn/code/adventofcode/index.php -y 2020 -d 2
- /usr/bin/php /home/dn/code/adventofcode/index.php -y 2020 -d 3
- /usr/bin/php /home/dn/code/adventofcode/index.php -y 2020 -d 4
- /usr/bin/php /home/dn/code/adventofcode/index.php -y 2020 -d 5
-
-
- *
- *
- *
- *
- */
