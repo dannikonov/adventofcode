c05b91ebfae41e58a6d0032140c7cc1d08b62fe7
Danil Nikonov <danil@seventest.ru>
2022-12-01 08:56:00 +0300

    feat: template Day*

---
+++ template.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace y2022;
+
+use Common\Common;
+
+class Day2 extends Common
+{
+
+    function __construct($year, $day)
+    {
+        parent::__construct($year, $day);
+        $this->set_answer([1 => -1, 2 => -1]);
+    }
+
+    protected function parse_input()
+    {
+//        return $this->split_by_line();
+    }
+
+    public function solution_1()
+    {
+//        $input = $this->parse_input();
+
+//        return $result;
+    }
+
+    public function solution_2()
+    {
+        //        $input = $this->parse_input();
+
+//        return $result;
+    }
+
+}
